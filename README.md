865c0cf281bfc6e910c767c96e2c018b14b1ff8d
Kenin <kenouen@hotmail.com>
2019-09-12 18:06:50 -0300

    roteiro4

--- README.md
+++
@@ -1,3 +0,0 @@
-# Grafos
-
-Roteiros de Grafos
--- TESTE.py
+++ TESTE.py
@@ -1,14 +1,18 @@
-from grafoPC import *
 
-g_p = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'],
-                 {'a1':'J-C', 'a2':'C-E', 'a3':'C-E', 'a4':'C-P', 'a5':'C-P', 'a6':'C-M', 'a7':'C-T', 'a8':'M-T', 'a9':'T-Z'})
 
-print(g_p.DFS('J'))
+V = input().split(",")
+a = input().split(",")
 
-grafo = Grafo(['A','B','C','D','E','F','G','H','I','J','K'],
-              {'1':'A-B', '2':'A-G','3':'A-J','4':'G-K','5':'K-J',
-              '6':'J-G','7':'J-I','8':'I-G', '9':'G-H','10':'H-F',
-              '11':'F-B','12':'B-G','13':'B-C','14':'C-D','15':'D-E',
-              '16':'D-B','17':'B-E'})
+M = []
+for k in range(len(V)):
+    M.append(list())
+    for l in range(len(V)):
+        if k > l:
+            M[k].append('-')
+        else:
+            M[k].append(0)
 
-print(grafo.DFS("K"))
+for i in a:
+    M[V.index(i[-3])][V.index(i[-1])] += 1
+
+print(M)
