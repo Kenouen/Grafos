466d59e4e2f2da62c2bba4398d5b0cf2644cd7d0
kenin <kenouen@hotmail.com>
2019-11-15 11:16:00 -0300

    mudança no master

---
+++ Roteiros/Roteiro_6.py
@@ -0,0 +1,258 @@
+class VerticeInvalidoException(Exception):
+    pass
+
+class ArestaInvalidaException(Exception):
+    pass
+
+class MatrizInvalidaException(Exception):
+    pass
+
+class Grafo:
+
+    QTDE_MAX_SEPARADOR = 1
+    SEPARADOR_ARESTA = '-'
+    __maior_vertice = 0
+
+    def __init__(self, V=None, A=None):
+        '''
+        Constrói um objeto do tipo Grafo. Se nenhum parâmetro for passado, cria um Grafo vazio.
+        Se houver alguma aresta ou algum vértice inválido, uma exceção é lançada.
+        :param V: Uma lista dos vértices (ou nodos) do grafo.
+        :param V: Uma matriz de adjacência que guarda as arestas do grafo. Cada entrada da matriz tem um inteiro que indica a quantidade de arestas que ligam aqueles vértices
+        '''
+
+        if V == None:
+            V = list()
+        if A == None:
+            A = dict()
+
+        for v in V:
+            if not(Grafo.verticeValido(v)):
+                raise VerticeInvalidoException('O vértice ' + v + ' é inválido')
+            if len(v) > self.__maior_vertice:
+                self.__maior_vertice = len(v)
+
+        self.N = list(V)
+        M = []
+
+        for k in range(len(V)):
+            M.append(list())
+            for l in range(len(V)):
+                M[k].append(0)
+        self.M = M
+        for i in A.values():
+            self.adicionaAresta(i)
+
+        if len(M) != len(V):
+            raise MatrizInvalidaException('A matriz passada como parâmetro não tem o tamanho correto')
+
+        for c in M:
+            if len(c) != len(V):
+                raise MatrizInvalidaException('A matriz passada como parâmetro não tem o tamanho correto')
+
+        for i in range(len(V)):
+            for j in range(len(V)):
+                '''
+                Verifica se os índices passados como parâmetro representam um elemento da matriz abaixo da diagonal principal.
+                Além disso, verifica se o referido elemento é um traço "-". Isso indica que a matriz é não direcionada e foi construída corretamente.
+                '''
+                # if i>j and not(M[i][j] == '-'):
+                #     raise MatrizInvalidaException('A matriz não representa uma matriz não direcionada')
+
+                aresta = V[i] + Grafo.SEPARADOR_ARESTA + V[j]
+                if not(self.arestaValida(aresta)):
+                    raise ArestaInvalidaException('A aresta ' + aresta + ' é inválida')
+
+        self.M = M
+        self.len = len(self.N)
+        self.quantVertices = len(self.N)
+
+    def arestaValida(self, aresta=''):
+        '''
+        Verifica se uma aresta passada como parâmetro está dentro do padrão estabelecido.
+        Uma aresta é representada por um string com o formato a-b, onde:
+        a é um substring de aresta que é o nome de um vértice adjacente à aresta.
+        - é um caractere separador. Uma aresta só pode ter um único caractere como esse.
+        b é um substring de aresta que é o nome do outro vértice adjacente à aresta.
+        Além disso, uma aresta só é válida se conectar dois vértices existentes no grafo.
+        :param aresta: A aresta que se quer verificar se está no formato correto.
+        :return: Um valor booleano que indica se a aresta está no formato correto.
+        '''
+
+        # Não pode haver mais de um caractere separador
+        if aresta.count(Grafo.SEPARADOR_ARESTA) != Grafo.QTDE_MAX_SEPARADOR:
+            return False
+
+        # Índice do elemento separador
+        i_traco = aresta.index(Grafo.SEPARADOR_ARESTA)
+
+        # O caractere separador não pode ser o primeiro ou o último caractere da aresta
+        if i_traco == 0 or aresta[-1] == Grafo.SEPARADOR_ARESTA:
+            return False
+
+        if not(self.existeVertice(aresta[:i_traco])) or not(self.existeVertice(aresta[i_traco+1:])):
+            return False
+
+        return True
+
+    @classmethod
+    def verticeValido(self, vertice: str):
+        '''
+        Verifica se um vértice passado como parâmetro está dentro do padrão estabelecido.
+        Um vértice é um string qualquer que não pode ser vazio e nem conter o caractere separador.
+        :param vertice: Um string que representa o vértice a ser analisado.
+        :return: Um valor booleano que indica se o vértice está no formato correto.
+        '''
+        return vertice != '' and vertice.count(Grafo.SEPARADOR_ARESTA) == 0
+
+    def existeVertice(self, vertice: str):
+        '''
+        Verifica se um vértice passado como parâmetro pertence ao grafo.
+        :param vertice: O vértice que deve ser verificado.
+        :return: Um valor booleano que indica se o vértice existe no grafo.
+        '''
+        return Grafo.verticeValido(vertice) and self.N.count(vertice) > 0
+
+    def __primeiro_vertice_aresta(self, a: str):
+        '''
+        Dada uma aresta no formato X-Y, retorna o vértice X
+        :param a: a aresta a ser analisada
+        :return: O primeiro vértice da aresta
+        '''
+        return a[0:a.index(Grafo.SEPARADOR_ARESTA)]
+
+    def __segundo_vertice_aresta(self, a: str):
+        '''
+        Dada uma aresta no formato X-Y, retorna o vértice Y
+        :param a: A aresta a ser analisada
+        :return: O segundo vértice da aresta
+        '''
+        return a[a.index(Grafo.SEPARADOR_ARESTA)+1:]
+
+    def __indice_primeiro_vertice_aresta(self, a: str):
+        '''
+        Dada uma aresta no formato X-Y, retorna o índice do vértice X na lista de vértices
+        :param a: A aresta a ser analisada
+        :return: O índice do primeiro vértice da aresta na lista de vértices
+        '''
+        return self.N.index(self.__primeiro_vertice_aresta(a))
+
+    def __indice_segundo_vertice_aresta(self, a: str):
+        '''
+        Dada uma aresta no formato X-Y, retorna o índice do vértice Y na lista de vértices
+        :param a: A aresta a ser analisada
+        :return: O índice do segundo vértice da aresta na lista de vértices
+        '''
+        return self.N.index(self.__segundo_vertice_aresta(a))
+
+    def existeAresta(self, a: str):
+        '''
+        Verifica se uma aresta passada como parâmetro pertence ao grafo.
+        :param aresta: A aresta a ser verificada
+        :return: Um valor booleano que indica se a aresta existe no grafo.
+        '''
+        existe = False
+        if Grafo.arestaValida(self, a):
+            for i in range(len(self.M)):
+                for j in range(len(self.M)):
+                    if self.M[self.__indice_primeiro_vertice_aresta(a)][self.__indice_segundo_vertice_aresta(a)]:
+                        existe = True
+
+    def adicionaAresta(self, a):
+        '''
+        Adiciona uma aresta ao grafo no formato X-Y, onde X é o primeiro vértice e Y é o segundo vértice
+        :param a: a aresta no formato correto
+        :raise: lança uma exceção caso a aresta não estiver em um formato válido
+        '''
+        if self.arestaValida(a):
+            i_a1 = self.__indice_primeiro_vertice_aresta(a)
+            i_a2 = self.__indice_segundo_vertice_aresta(a)
+            self.M[i_a1][i_a2] += 1
+        else:
+            raise ArestaInvalidaException('A aresta {} é inválida'.format(a))
+
+    def __str__(self):
+        '''
+        Fornece uma representação do tipo String do grafo.
+        O String contém um sequência dos vértices separados por vírgula, seguido de uma sequência das arestas no formato padrão.
+        :return: Uma string que representa o grafo
+        '''
+
+        # Dá o espaçamento correto de acordo com o tamanho do string do maior vértice
+        espaco = ' '*(self.__maior_vertice)
+
+        grafo_str = espaco + ' '
+
+        for v in range(len(self.N)):
+            grafo_str += self.N[v]
+            if v < (len(self.N) - 1):  # Só coloca o espaço se não for o último vértice
+                grafo_str += ' '
+
+        grafo_str += '\n'
+
+        for l in range(len(self.M)):
+            grafo_str += self.N[l] + ' '
+            for c in range(len(self.M)):
+                grafo_str += str(self.M[l][c]) + ' '
+            grafo_str += '\n'
+        return grafo_str
+
+
+
+# ======================================================================================================================
+#                                                       ROTEIRO 5
+# ======================================================================================================================
+
+    def copy(self, m):
+        aux = []
+        for i in range(self.len):
+            aux.append([])
+            for p in range(self.len):
+                aux[i].append(m[i][p])
+        return aux
+
+# ======================================================================================================================
+#                                                       ROTEIRO 6
+# ======================================================================================================================
+
+    def warshall(self):
+        E = self.copy(self.M)
+        for i in range(0, self.len):
+            for j in range(0, self.len):
+                if E[j][i] == 1:
+                    for k in range(0, self.len):
+                        E[j][k] = max(E[j][k], E[i][k])
+
+        # Dá o espaçamento correto de acordo com o tamanho do string do maior vértice
+        espaco = ' ' * (self.__maior_vertice)
+
+        grafo_str = espaco + ' '
+
+        for v in range(len(self.N)):
+            grafo_str += self.N[v]
+            if v < (len(self.N) - 1):  # Só coloca o espaço se não for o último vértice
+                grafo_str += ' '
+
+        grafo_str += '\n'
+
+        for l in range(self.len):
+            grafo_str += self.N[l] + ' '
+            for c in range(len(E)):
+                grafo_str += str(E[l][c]) + ' '
+            grafo_str += '\n'
+        return grafo_str
+
+
+# ======================================================================================================================
+#                                                       Testes
+# ======================================================================================================================
+
+
+g = Grafo(['A', 'B', 'C', 'D'], {'a1': 'A-B', 'a2':'A-C', 'a3':'B-C', 'a4':'C-D'})
+
+print(g.warshall())
+
+g_p = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'],
+                 {'a1': 'J-C', 'a2': 'C-E', 'a3': 'E-C', 'a4': 'C-P', 'a5': 'P-C', 'a6': 'C-M','a7': 'T-C', 'a8': 'M-T', 'a9': 'T-Z'})
+
+print(g_p.warshall())
---
+++ Roteiros/Roteiro_7.py
@@ -0,0 +1,207 @@
+import unittest
+class VerticeInvalidoException(Exception):
+    pass
+
+class ArestaInvalidaException(Exception):
+    pass
+
+class Grafo:
+
+    QTDE_MAX_SEPARADOR = 1
+    SEPARADOR_ARESTA = '-'
+
+    def __init__(self, N=[], A={}):
+        '''
+        Constrói um objeto do tipo Grafo. Se nenhum parâmetro for passado, cria um Grafo vazio.
+        Se houver alguma aresta ou algum vértice inválido, uma exceção é lançada.
+        :param N: Uma lista dos vértices (ou nodos) do grafo.
+        :param V: Uma dicionário que guarda as arestas do grafo. A chave representa o nome da aresta e o valor é uma string que contém dois vértices separados por um traço.
+        '''
+        for v in N:
+            if not(Grafo.verticeValido(v)):
+                raise VerticeInvalidoException('O vértice ' + v + ' é inválido')
+
+        self.N = N
+
+        for a in A:
+            if not(self.arestaValida(A[a])):
+                raise ArestaInvalidaException('A aresta ' + A[a] + ' é inválida')
+        self.len = len(N)
+        self.A = A
+
+    def arestaValida(self, aresta=''):
+        '''
+        Verifica se uma aresta passada como parâmetro está dentro do padrão estabelecido.
+        Uma aresta é representada por um string com o formato a-b, onde:
+        a é um substring de aresta que é o nome de um vértice adjacente à aresta.
+        - é um caractere separador. Uma aresta só pode ter um único caractere como esse.
+        b é um substring de aresta que é o nome do outro vértice adjacente à aresta.
+        Além disso, uma aresta só é válida se conectar dois vértices existentes no grafo.
+        :param aresta: A aresta que se quer verificar se está no formato correto.
+        :return: Um valor booleano que indica se a aresta está no formato correto.
+        '''
+
+        # Não pode haver mais de um caractere separador
+        if aresta.count(Grafo.SEPARADOR_ARESTA) != Grafo.QTDE_MAX_SEPARADOR:
+            return False
+
+        # Índice do elemento separador
+        i_traco = aresta.index(Grafo.SEPARADOR_ARESTA)
+
+        # O caractere separador não pode ser o primeiro ou o último caractere da aresta
+        if i_traco == 0 or aresta[-1] == Grafo.SEPARADOR_ARESTA:
+            return False
+
+        # Verifica se as arestas antes de depois do elemento separador existem no Grafo
+        if not(self.existeVertice(aresta[:i_traco])) or not(self.existeVertice(aresta[i_traco+1:])):
+            return False
+
+        return True
+
+    @classmethod
+    def verticeValido(self, vertice=''):
+        '''
+        Verifica se um vértice passado como parâmetro está dentro do padrão estabelecido.
+        Um vértice é um string qualquer que não pode ser vazio e nem conter o caractere separador.
+        :param vertice: Um string que representa o vértice a ser analisado.
+        :return: Um valor booleano que indica se o vértice está no formato correto.
+        '''
+        return vertice != '' and vertice.count(Grafo.SEPARADOR_ARESTA) == 0
+
+    def existeVertice(self, vertice=''):
+        '''
+        Verifica se um vértice passado como parâmetro pertence ao grafo.
+        :param vertice: O vértice que deve ser verificado.
+        :return: Um valor booleano que indica se o vértice existe no grafo.
+        '''
+        return Grafo.verticeValido(vertice) and self.N.count(vertice) > 0
+
+    def existeAresta(self, aresta=''):
+        '''
+        Verifica se uma aresta passada como parâmetro pertence ao grafo.
+        :param aresta: A aresta a ser verificada
+        :return: Um valor booleano que indica se a aresta existe no grafo.
+        '''
+        existe = False
+        if Grafo.arestaValida(self, aresta):
+            for k in self.A:
+                if aresta == self.A[k]:
+                    existe = True
+
+        return existe
+
+    def adicionaVertice(self, v):
+        '''
+        Adiciona um vértice no Grafo caso o vértice seja válido e não exista outro vértice com o mesmo nome
+        :param v: O vértice a ser adicionado
+        :raises: VerticeInvalidoException se o vértice passado como parâmetro não puder ser adicionado
+        '''
+        if self.verticeValido(v) and not self.existeVertice(v):
+            self.N.append(v)
+        else:
+            raise VerticeInvalidoException('O vértice ' + v + ' é inválido')
+
+    def adicionaAresta(self, nome, a):
+        '''
+        Adiciona uma aresta no Grafo caso a aresta seja válida e não exista outra aresta com o mesmo nome
+        :param v: A aresta a ser adicionada
+        :raises: ArestaInvalidaException se a aresta passada como parâmetro não puder ser adicionada
+        '''
+        if self.arestaValida(a):
+            self.A[nome] = a
+        else:
+            ArestaInvalidaException('A aresta ' + self.A[a] + ' é inválida')
+
+    def arestas_sobre_vertice(self, d, lista):
+
+        '''
+        Resposta da E da Segunda questão pro primeiro roteiro " Quais arestas incidem sobre um vértice N arbitrário?"
+        '''
+
+        arestas = []
+        for i in lista:
+            if d in i:
+                if d == i[0]:
+                    arestas.append(i)
+                else:
+                    arestas.append(i[::-1])
+        return arestas
+
+    def remove_parallel(self):
+        temp = []
+        for i in self.A.values():
+            if i not in temp and i[0] != i[-1] and i[::-1] not in temp:
+                temp.append(i)
+        return temp
+
+    def DijkstraHead(self):
+        temp = self.remove_parallel()
+        mapa = {}
+        for i in self.N:
+            arestas = self.arestas_sobre_vertice(i, temp)
+            mapa[i] = [arestas, False, len(self.A.values()) + 1, '']
+        return mapa
+
+
+    def Dijkstra(self, w, v, gasosa):
+        u = w
+        dados = self.DijkstraHead()
+        #dados[vertice] = [arestas, Fi, Beta, pi]
+
+        fis = []
+        # fi de u vira 1
+        dados[u][1] = True
+
+        # Beta de u = 0
+        dados[u][2] = 0
+
+        while u != v and len(fis) < self.len:
+
+            #Para todos os outros vértices 𝞫(r) ⇽ ∞, 𝞿(r) ⇽ 0, 𝞹(r) ⇽ 0 e w ⇽ u
+            for i in dados[u][0]:
+                if dados[i[-1]][2] >= dados[u][2] + 1:
+                    dados[i[-1]][2] = dados[u][2] + 1
+                    dados[i[-1]][3] = u
+
+
+            #Ache um vértice r* tal que 𝞿(r*)=0, 𝞫(r*)<∞ e 𝞫(r*)=min𝞿(r) = 0(𝞫(r))
+            menorcaminho = len(self.A.values()) + 1
+            menorvertice = ''
+            for i in dados.keys():
+                aux = dados[i]
+                if aux[1] == False and aux[2] < menorcaminho and gasosa >= 0:
+                    menorcaminho = aux[2]
+                    menorvertice = i
+
+            #Faça 𝞿(r*) = 1 e w = r*
+
+            dados[menorvertice][1] = True
+            u = menorvertice
+            gasosa -= 1
+            if u == "E":
+                gasosa = 5
+            #Se r* não existe, não há caminho u-v e o algoritmo deve parar
+            if u == '':
+                break
+
+        aux = []
+        aux.append(v)
+        while v != w:
+            t = dados[v][3]
+            aux.append(t)
+            v = t
+
+        for i in aux[::-1]:
+            print(i,end=' ')
+            if(i != u):
+                print("-", end=' ')
+
+
+
+g_p = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'],
+                 {'a1':'J-C', 'a2':'C-E', 'a3':'E-C', 'a4':'C-P', 'a5':'C-P', 'a6':'C-M', 'a8':'M-T', 'a9':'T-Z'})
+
+g_p.Dijkstra('C', 'Z', 3)
+
+
+grafo = Grafo(['A''B''C''D''E''F''G''H''I''J''K''L''M''N''O''P''Q''R''S''T''U''V''X''W''Y''Z''a''b''c''d''e''f'], { 'a0' : 'A-B','a1' : 'A-C','a2' : 'A-D','a3' : 'B-C','a4' : 'B-E','a5' : 'C-F','a6' : 'D-H','a7' : 'D-L','a8' : 'E-I','a9' : 'E-F','a10' : 'F-G','a11' : 'F-J','a12' : 'F-K','a13' : 'G-K','a14' : 'G-D','a15' : 'H-G','a16' : 'I-M','a17' : 'J-N','a18' : 'K-L','a19' : 'K-O','a20' : 'L-P', 'a21' : 'M-Q','a22' : 'N-R','a23' : 'O-R','a24' : 'O-Q','a25' : 'O-S','a26' : 'P-R','a27' : 'P-T','a28' : 'R-U','a29' : 'R-S','a30' : 'S-X','a31' : 'S-T','a32' : 'T-Y','a33' : 'U-W','a34' : 'U-Z','a35' : 'V-R','a36' : 'X-V','a37' : 'X-b','a38' : 'X-c','a39' : 'Y-c','a40' : 'Y-d','a41' : 'a-V','a42' : 'd-g','a43' : 'g-f','a44' : 'f-e','a45' : 'f-h'}]
